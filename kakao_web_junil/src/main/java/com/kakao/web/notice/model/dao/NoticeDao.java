0fdcd4cc4cd2f6af630fa748f817637b413055d7
junil <skjil1218@gmail.com>
2021-10-13 17:56:49 +0900

    notice 관련 api 추가

---
+++ kakao_web_junil/src/main/java/com/kakao/web/notice/model/dao/NoticeDao.java
@@ -0,0 +1,9 @@
+package com.kakao.web.notice.model.dao;
+
+import java.util.List;
+
+import com.kakao.web.notice.model.dto.Notice;
+
+public interface NoticeDao {
+	public List<Notice> getNoticeAll();
+}
